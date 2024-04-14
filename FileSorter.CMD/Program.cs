e4efd0d902e3c8e6872cbc775c117cffe0e8e869
AntonSkuratov <164427347+AntonSkuratov@users.noreply.github.com>
2024-04-14 03:12:14 +0300

    Delete FileSorter.CMD directory

--- FileSorter.CMD/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using FileSorter.BL.Models;
-List<string> list = new List<string>();
-list.Add("C:\\Users\\anton\\OneDrive\\Рабочий стол\\Test1");
-var a = "C:\\Users\\anton\\OneDrive\\Рабочий стол\\Tests";
-FileRepository fileRepository=new FileRepository(list);
-FileRepositoryManagement repositoryManagement = new FileRepositoryManagement(fileRepository);
-repositoryManagement.MoveTo(a);
